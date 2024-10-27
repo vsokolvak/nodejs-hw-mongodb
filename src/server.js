83d6ae4f885ebb51d3923be1529023ca605da52f
Vadim <vsokolvak2000@gmail.com>
2024-10-27 17:59:06 +0200

    add port alternative

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import { env } from './env.js';
 import { getAllContacts, getContactsById } from './services/contacts.js';
 
-const PORT = Number(env('PORT'));
+const PORT = Number(env('PORT')) || 3000;
 
 export const setupServer = () => {
   const app = express();
