1b3e96a691313ad46fd74979bb5ca6e01618a647
Vadim <vsokolvak2000@gmail.com>
2024-11-10 15:06:36 +0200

    change imported from /opt/render/project/src/src/utils/parseSortParams.js

--- src/utils/parseSortParams.js
+++ src/utils/parseSortParams.js
@@ -1,4 +1,4 @@
-import { SORT_ORDER } from "../constants";
+import { SORT_ORDER } from "../constants.js";
 
 const parseSortOrder = sortOrder => {
   const isKnownOrder = [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder);
