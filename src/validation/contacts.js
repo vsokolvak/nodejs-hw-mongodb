754afce4dff52182c15ba32f2f901feaa012f4a1
Vadim <vsokolvak2000@gmail.com>
2024-11-11 12:18:26 +0200

    fixed rules validation number

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,8 +3,9 @@ import { contactsType } from "../db/models/contacts.js";
 
 export const createContactsShema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
-  phoneNumber: Joi.number().integer().min(5).max(12).required(),
+  phoneNumber: Joi.number().integer().min(10000).max(999999999999).required(),
   email: Joi.string().email().required(),
   isFavourite: Joi.boolean().default(false),
   contactType: Joi.string().valid(...contactsType).required()
 });
+
